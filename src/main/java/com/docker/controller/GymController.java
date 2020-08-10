4d348d3e1a383dfe46a1110877357d54944dda86
rajvpatil5 <rajvpatil5@gmail.com>
2020-08-10 16:48:40 +0530

    add landing page

---
+++ src/main/java/com/docker/controller/GymController.java
@@ -0,0 +1,17 @@
+package com.docker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GymController {
+
+	@GetMapping("/")
+	public String homePage() {
+		return "index";
+	}
+	@GetMapping("/home")
+	public String home1Page() {
+		return "home";
+	}
+}
