cfe9afc48a15dcc6482d6e2c834f74b52449b037
rajvpatil5 <rajvpatil5@gmail.com>
2020-08-11 22:11:21 +0530

    new dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM openjdk:8
 ADD target/docker-springboot.jar docker-springboot.jar
 EXPOSE 7070
 ENTRYPOINT [ "java", "-jar", "docker-springboot.jar" ]
+ENV dbDriver=com.mysql.jdbc.Driver dbConnectionUrl=jdbc:mysql://mysql-spring:3306/test dbUserName=root dbPassword=root
--- target/classes/schema-mysql.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `test`.`message` (
-  `message_id` INT NOT NULL AUTO_INCREMENT,
-  `full_name` VARCHAR(45) NULL,
-  `emailid` VARCHAR(45) NULL,
-  `mobile_number` VARCHAR(45) NULL,
-  `message_text` VARCHAR(500) NULL,
-  PRIMARY KEY (`message_id`))
-ENGINE = InnoDB;
